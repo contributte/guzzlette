4fc1481d125f573caf483c7646ae7c11c3b35718
Martin Macho <matyxcz@gmail.com>
2016-07-25 15:46:49 +0200

    Guzzlete: turn on only if debug bar is enabled

--- src/Matyx/Guzzlette/Guzzlette.php
+++ src/Matyx/Guzzlette/Guzzlette.php
@@ -18,39 +18,42 @@ class Guzzlette {
 	public static function createGuzzleClient($tempDir, $guzzleConfig = []) {
 		if(isset(static::$client)) return static::$client;
 
-		$handler = NULL;
-		if(isset($guzzleConfig['handler'])) {
-			$handler = $guzzleConfig['handler'];
-			if(!($handler instanceof GuzzleHttp\HandlerStack)) {
-				throw new GuzzletteException("Handler must be instance of " . GuzzleHttp\HandlerStack::class);
+		if(Tracy\Debugger::isEnabled()) {
+			$handler = NULL;
+			if(isset($guzzleConfig['handler'])) {
+				$handler = $guzzleConfig['handler'];
+				if(!($handler instanceof GuzzleHttp\HandlerStack)) {
+					throw new GuzzletteException("Handler must be instance of " . GuzzleHttp\HandlerStack::class);
+				}
 			}
+			else {
+				$handler = GuzzleHttp\HandlerStack::create();
+			}
+
+			$requestStack = new RequestStack();
+
+			Tracy\Debugger::getBar()->addPanel(new TracyPanel($tempDir, $requestStack));
+
+			$handler->push(function (callable $handler) use ($requestStack) {
+				return function ($request, array $options) use ($handler, $requestStack) {
+					Tracy\Debugger::timer();
+
+					$guzzletteRequest = new Request();
+					$guzzletteRequest->request = $request;
+
+					return $handler($request, $options)->then(function ($response) use ($requestStack, $guzzletteRequest) {
+						$guzzletteRequest->time = Tracy\Debugger::timer();
+						$guzzletteRequest->response = $response;
+
+						$requestStack->addRequest($guzzletteRequest);
+
+						return $response;
+					});
+				};
+			});
+
+			$guzzleConfig['handler'] = $handler;
 		}
-		else {
-			$handler = GuzzleHttp\HandlerStack::create();
-		}
-
-		$requestStack = new RequestStack();
-		Tracy\Debugger::getBar()->addPanel(new TracyPanel($tempDir, $requestStack));
-
-		$handler->push(function (callable $handler) use ($requestStack) {
-			return function ($request, array $options) use ($handler, $requestStack) {
-				Tracy\Debugger::timer();
-
-				$guzzletteRequest = new Request();
-				$guzzletteRequest->request = $request;
-
-				return $handler($request, $options)->then(function ($response) use ($requestStack, $guzzletteRequest) {
-					$guzzletteRequest->time = Tracy\Debugger::timer();
-					$guzzletteRequest->response = $response;
-
-					$requestStack->addRequest($guzzletteRequest);
-
-					return $response;
-				});
-			};
-		});
-
-		$guzzleConfig['handler'] = $handler;
 
 		static::$client = new GuzzleHttp\Client($guzzleConfig);
 
